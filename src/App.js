66128d502d7887517a5fb39031e8cd123bae4889
Jiri Chara <jiri@chara.vip>
2017-07-12 16:01:31 +0200

    Add bulma and react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
 import './App.css';
+import logo from './assets/images/logo.png';
 
 export default () => (
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
+  <Router>
+    <div className="container">
+      <nav className="navbar">
+        <a className="navbar-item">
+          <img src={logo} alt="AppDirect" />
+        </a>
+
+        <div id="navMenuExample" className="navbar-menu">
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/">
+              Home
+            </Link>
+
+            <Link className="navbar-item" to="/about">
+              About
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  </Router>
 );
